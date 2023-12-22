2af6131e1f365e20c397a8a14369440a21d226b4
dnwlr <dnwlr@DESKTOP-L8GO68Q>
2023-12-22 15:41:31 +0900

    First commit in dev branch

--- GitHW/src/mygit/MyClass.java
+++ GitHW/src/mygit/MyClass.java
@@ -5,6 +5,8 @@ public class MyClass {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
 		System.out.println("This is the 2nd version");
+		YourClass your = new YourClass();
+		your.sayHello("Everyone");;
 	}
 
 }
